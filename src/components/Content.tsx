5c1b02bb9d1282170fc6d6046693b1729cc70bb3
Drianka Mahdy Adimas <mahdyadi@gmail.com>
2019-07-31 17:40:11 +0700

    Added basic code organization, the template of future code should be like current commit

---
+++ src/components/Content.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * 
+ */
+export default class Content extends React.Component {
+    
+    render(){
+        return (
+            <div className="content">
+                <p>content</p>
+            </div>
+        )
+    }
+}
